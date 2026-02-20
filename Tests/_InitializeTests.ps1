8c1b5dd0f03d6f936dc107518168ec7a00793faf
Jonathan Colon <rebelinux@gmail.com>
2026-02-20 11:44:20 -0400

    Refactor Pester workflow: improve job configuration and enhance assembly loading in tests

--- Tests/_InitializeTests.ps1
+++ Tests/_InitializeTests.ps1
@@ -26,3 +26,28 @@ $Script:GraphvizPath = switch ($PSVersionTable.Platform) {
 if (-not (Get-Module $ModuleName)) {
     Import-Module $ModuleRoot -Force
 }
+
+$assemblyName = switch ($PSVersionTable.PSEdition) {
+    'Core' {
+        @(Get-ChildItem -Path ("$ProjectRoot{0}Src{0}Bin{0}Assemblies{0}net90{0}*.dll" -f [System.IO.Path]::DirectorySeparatorChar) -ErrorAction SilentlyContinue)
+    }
+    'Desktop' {
+        @(Get-ChildItem -Path ("$ProjectRoot{0}Src{0}Bin{0}Assemblies{0}net48{0}*.dll" -f [System.IO.Path]::DirectorySeparatorChar) -ErrorAction SilentlyContinue)
+    }
+    default {
+        @()
+    }
+}
+
+$loadedassemblies = [System.AppDomain]::CurrentDomain.GetAssemblies().ManifestModule.Name
+
+foreach ($Assembly in $assemblyName) {
+    if ($Assembly.Name -notin $loadedassemblies) {
+        try {
+            Write-Verbose -Message "Loading assembly '$($Assembly.Name)'."
+            Add-Type -Path $Assembly.FullName -Verbose
+        } catch {
+            Write-Error -Message "Failed to add assembly $($Assembly.FullName): $_"
+        }
+    }
+}
