b6673a0b99f181f85ceae511b8008074b3697973
Jonathan Colon <rebelinux@gmail.com>
2026-02-22 20:49:41 -0400

    Add image processing features and project structure for PDF conversion
    - Implement ConvertPngToPdf functionality to convert PNG images to PDF format.
    - Create ImageProcessor class for image manipulation tasks including resizing, rotating, and adding watermarks.
    - Set up project files and solution structure for DiaConvertImageToPDF and Diagrammer modules.
    - Update .gitignore to exclude unnecessary files and directories.
    - Add TODO for future support of watermark in SVG output format.

---
+++ Sources/DiaConvertImageToPDF/DiaConverterImageToPDF.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DiaConvertImageToPDF
+{
+    public class MainEntry
+    {
+        public static void Main(string[] args)
+        {
+            // ConvertImageToPDF.ConvertPngToPdf("/home/rebelinux/foto.png", "/home/rebelinux/foto.pdf");
+        }
+    }
+}
