3e8966aa99f92f8ea20224147670073c40c24472
Jonathan Colon <rebelinux@gmail.com>
2026-08-03 10:42:14 -0400

    Fix subgraph hidden node

--- AsBuiltReport.Diagram/Src/Public/New-AbrDiagram.ps1
+++ AsBuiltReport.Diagram/Src/Public/New-AbrDiagram.ps1
@@ -675,7 +675,7 @@ function New-AbrDiagram {
         foreach ($OutputFormat in $Format) {
             #Export the Diagram
             if ($Graph) {
-                $FilteredGraph = $Graph | Select-String -Pattern '(?s)"?\w+"?\s+\[\s*label="";\s*shape="point";\s*style="invis";\s*\]' -NotMatch
+                $FilteredGraph = $Graph | Select-String -Pattern '(?s)"?\w+"?\s+\[\s*label="";\s*shape="point";\s*style="invis";\s*\]' -NotMatch | Select-String -Pattern '(?s)"?\w+"?\s+\[\s*label="",\s*shape="point",\s*style="invis",\s*\]' -NotMatch | Select-String -Pattern '(?s)"?\w+"?\s+\[\s*label="",\s*shape=point,\s*style=invis,\s*\]' -NotMatch
                 Export-AbrDiagram -GraphObj $FilteredGraph -ErrorDebug $EnableErrorDebug -Format $OutputFormat -Filename "$Filename.$OutputFormat" -OutputFolderPath $OutputFolderPath -WaterMarkText $WaterMarkText -WaterMarkColor $WaterMarkColor -IconPath $IconPath -WaterMarkFontOpacity $WaterMarkFontOpacity
             } else {
                 Write-Verbose -Message 'No Graph object found. Disabling diagram section'
